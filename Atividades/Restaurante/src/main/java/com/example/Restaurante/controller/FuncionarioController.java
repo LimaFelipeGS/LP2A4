546f1ebfeca5f1865a09478677e8ca25fa6cba65
LimaFelipeGS <limafelipegs@gmail.com>
2023-09-20 15:05:23 -0300

    Restaurante - Versão Final

--- Atividades/Restaurante/src/main/java/com/example/Restaurante/controller/FuncionarioController.java
+++ Atividades/Restaurante/src/main/java/com/example/Restaurante/controller/FuncionarioController.java
@@ -1,6 +1,8 @@
 package com.example.Restaurante.controller;
 
+import com.example.Restaurante.dto.FuncionarioRequestDTO;
 import com.example.Restaurante.dto.FuncionarioResponseDTO;
+import com.example.Restaurante.model.Funcionario;
 import com.example.Restaurante.repository.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +23,21 @@ public class FuncionarioController {
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
-    public void saveFuncionario()
+    public void saveFuncionario(@RequestBody FuncionarioRequestDTO data) {
+        repository.save(new Funcionario(data));
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PutMapping("/{id}")
+    public void editarFuncinario(@PathVariable Long id, @RequestBody FuncionarioRequestDTO data) {
+        Funcionario funcionario = new Funcionario(data);
+        funcionario.setId(id);
+        repository.save(funcionario);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteFuncionario(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
 
 }
